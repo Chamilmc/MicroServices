5230d7ec0358a348d85336858ee3f43e623e09c4
Mohottige Chamil Asanka <chamil.asanka@xtractify.ai>
2024-07-18 17:26:19 +0530

    Add migration and API contoller Add web project and add coupon index

---
+++ Mongo.Web/Controllers/CouponController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Mongo.Web.Models;
+using Mongo.Web.Service.IService;
+using Newtonsoft.Json;
+
+namespace Mongo.Web.Controllers
+{
+    public class CouponController : Controller
+    {
+        private readonly ICouponService _couponService;
+
+        public CouponController(ICouponService couponService)
+        {
+            _couponService = couponService;
+        }
+
+        public async Task<IActionResult> CouponIndex()
+        {
+            List<CouponDto> list = new();
+
+            ResponseDto? response = await _couponService.GetAllCouponAsync();
+
+            if (response != null && response.IsSuccess)
+            {
+                list = JsonConvert.DeserializeObject<List<CouponDto>>(Convert.ToString(response.Result));
+            }
+            return View(list);
+        }
+    }
+}
---
+++ Mongo.Web/Service/BaseService.cs
@@ -0,0 +1,82 @@
+﻿using Mongo.Web.Models;
+using Mongo.Web.Service.IService;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace Mongo.Web.Service
+{
+    public class BaseService : IBaseService
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public BaseService(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<ResponseDto?> SendAsync(RequestDto requestDto)
+        {
+            try
+            {
+
+                HttpClient httpClient = _httpClientFactory.CreateClient("MongoAPI");
+                HttpRequestMessage message = new();
+                message.Headers.Add("Accept", "application/json");
+
+                // Token
+                message.RequestUri = new Uri(requestDto.Url);
+                if (requestDto.Data != null)
+                {
+                    message.Content = new StringContent(JsonConvert.SerializeObject(requestDto.Data), Encoding.UTF8, "application/json");
+                }
+
+                HttpResponseMessage? apiResponse = null;
+
+
+                switch (requestDto.ApiType)
+                {
+                    case Utility.SD.ApiType.POST:
+                        message.Method = HttpMethod.Post;
+                        break;
+                    case Utility.SD.ApiType.DELETE:
+                        message.Method = HttpMethod.Delete;
+                        break;
+                    case Utility.SD.ApiType.PUT:
+                        message.Method = HttpMethod.Put;
+                        break;
+                    default:
+                        message.Method = HttpMethod.Get;
+                        break;
+                }
+
+                apiResponse = await httpClient.SendAsync(message);
+
+                switch (apiResponse.StatusCode)
+                {
+                    case System.Net.HttpStatusCode.NotFound:
+                        return new() { IsSuccess = false, Message = "Not Found" };
+                    case System.Net.HttpStatusCode.Forbidden:
+                        return new() { IsSuccess = false, Message = "Access Denied" };
+                    case System.Net.HttpStatusCode.Unauthorized:
+                        return new() { IsSuccess = false, Message = "Unauthorized" };
+                    case System.Net.HttpStatusCode.InternalServerError:
+                        return new() { IsSuccess = false, Message = "Internal Server Error" };
+                    default:
+                        var apiContent = await apiResponse.Content.ReadAsStringAsync();
+                        var apiResponseDto = JsonConvert.DeserializeObject<ResponseDto>(apiContent);
+                        return apiResponseDto;
+                }
+            }
+            catch (Exception ex)
+            {
+                var dto = new ResponseDto
+                {
+                    Message = ex.Message.ToString(),
+                    IsSuccess = false
+                };
+
+                return dto;
+            }
+        }
+    }
+}
